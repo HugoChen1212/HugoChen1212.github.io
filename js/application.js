6932b424806d1ca4ba67c0ac5b067561a08674ed
Hugo <hugochen1212@gmail.com>
2017-10-29 00:59:39 -0700

    update contact with form

--- js/application.js
+++ js/application.js
@@ -7,3 +7,24 @@ $(document).ready(function() {
       }, 1000);
   });
 });
+
+$('#contact-form').submit(function(e){
+  var name = document.getElementById('inputName'),
+      email = document.getElementById('inputEmail'),
+      note = document.getElementById('inputNote');
+
+  if (!name.value || !email.value || !note.value) {
+      alertify.error('Please check your entries')
+  } else{
+    $.ajax({
+      url: "https://formspree.io/hugochen1212@gmail.com",
+      method: "POST",
+      data: $(this).serialize(),
+      dataType: "json"
+    });
+
+    e.preventDefault()
+    $(this).get(0).reset()
+    alertify.success('Message send')
+  }
+})
